8616ce6d8754837b7ef7f435ce3998aa1ff96745
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2024-10-04 08:44:33 +0530

    Add files via upload

---
+++ Array/03 Zeroth Element of Array/zeroth Element of Array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[4]={};
+    printf("\n This is Value of 1st Element=%d",Num[0]);
+    printf("\n This is Value of 2nd Element=%d",Num[1]);
+    printf("\n This is Value of 3rd Element=%d",Num[2]);
+    printf("\n This is Value of 4th Element=%d",Num[3]);
+    printf("\n===============THANKS====================");
+    getch();
+    return 0;
+}
---
+++ Array/05  Array of Character Datatype/Array of Character Datatype.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Char[5]={'A','$','V','*','E'};
+    printf("\n This is 1st Element=%c",Char[0]);
+    printf("\n This is 2nd Element=%c",Char[1]);
+    printf("\n This is 3rd Element=%c",Char[2]);
+    printf("\n This is 4th Element=%c",Char[3]);
+    printf("\n This is 5th Element=%c",Char[4]);
+    printf("\n============THANKS==============");
+    getch();
+    return 0;
+}
