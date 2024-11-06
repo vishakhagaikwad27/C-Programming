8b7eecf4d10a5c08e7b8146c1e2c62062bddcd77
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2024-11-06 13:29:29 +0530

    Add files via upload

---
+++ Array/12 BillCount Without Malloc/BillCount Withot Malloc.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+    int i=0,Bills[7]={0};
+
+    printf("\n Enter Todays Bills...");
+
+    for(i=0; i<7; i++)
+    {
+        printf("\n Enter Bill No %d=",i+1);
+        scanf("%d",&Bills[i]);
+    }
+    getch();
+    system("cls");          ///clrscr(); Function to clear output window
+    printf("\n Bills in Array are....");
+
+    for(i=0; i<7; i++)
+    {
+        printf("\n Value of %d Bill=%d",i+101,Bills[i]);
+    }
+    printf("\n============THANKS============");
+    getch();
+    return 0;
+
+}
