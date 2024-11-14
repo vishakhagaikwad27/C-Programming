15ce604e5d5e62c3acc98260b0a36ecd46442700
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2024-11-14 11:37:12 +0530

    Add files via upload

---
+++ Basic C Program/09 Display Welcome/Display Welcome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+    printf("\n Welcome");
+
+    printf("\n========THANKS==========");
+    getch();
+    return 0;
+}
