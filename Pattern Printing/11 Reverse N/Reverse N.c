4885a28d1f29a9ef1e0dabeca837ed66e71d17ed
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2024-11-22 21:54:13 +0530

    Add files via upload

---
+++ Pattern Printing/11 Reverse N/Reverse N.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int R=0, C=0, X=0;
+    printf("\n Enter Row and Column Size=");
+    scanf("%d",&X);
+    printf("\n=======Pattern==========\n\n");
+    for(R=1; R<=X; R++)
+    {
+        for(C=1; C<=X; C++)
+        {
+            if(C==1 || C==X || R+C==X+1)
+            {
+                printf(" *");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n==========THANKS==========");
+    getch();
+    return 0;
+}
