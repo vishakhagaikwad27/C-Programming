86e3f26893135323febfbb6a3d51c39f7673297f
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2024-12-24 22:10:37 +0530

    Add files via upload

---
+++ Basic C Program/21 To Print Week Days/To Print Week Days.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n Monday");
+    printf("\n Tuesday");
+    printf("\n Wednesday");
+    printf("\n Thursday");
+    printf("\n Friday");
+    printf("\n Saturday");
+    printf("\n Sunday");
+    printf("\n ========THANKS========");
+    getch();
+    return 0;
+}
