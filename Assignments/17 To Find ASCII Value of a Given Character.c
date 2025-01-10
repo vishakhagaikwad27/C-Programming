e49481b18b4ea1ce36c370c7940dfba4ebd6923a
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-01-10 22:57:41 +0530

    Add files via upload

---
+++ Assignments/17 To Find ASCII Value of a Given Character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+    printf("\n Enter any Character=");
+    scanf("%c",&ch);
+
+    printf("ASCII Value of Given Character=%d",ch);
+
+    getch();
+    return 0;
+}
