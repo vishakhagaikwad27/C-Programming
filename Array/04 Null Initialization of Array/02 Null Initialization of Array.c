7e7d96c65b472cb78cde54c385d5a618a70ae5fe
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-01-14 19:29:49 +0530

    Add files via upload

---
+++ Array/04 Null Initialization of Array/02 Null Initialization of Array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[5]={0};
+    printf("\n This is The Value of 1st Element=%d",Num[0]);
+    printf("\n This is The Value of 2nd Element=%d",Num[1]);
+    printf("\n This is The Value of 3rd Element=%d",Num[2]);
+    printf("\n This is The Value of 4th Element=%d",Num[3]);
+    printf("\n This is The Value of 5th Element=%d",Num[4]);
+
+    printf("\n============THANKS==============");
+    getch();
+    return 0;
+
+
+}
---
+++ Array/04 Null Initialization of Array/03 Null Initialization of Array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num[5]={0,0,0,0,0};
+    printf("\n Value of 1st Element=%d",Num[0]);
+    printf("\n Value of 2nd Element=%d",Num[1]);
+    printf("\n Value of 3rd Element=%d",Num[2]);
+    printf("\n Value of 4th Element=%d",Num[3]);
+    printf("\n Value of 5th Element=%d",Num[4]);
+    printf("\n ============THANKS============");
+    getch();
+    return 0;
+}
