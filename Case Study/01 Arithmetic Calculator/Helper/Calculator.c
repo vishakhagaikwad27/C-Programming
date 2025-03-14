8f871e6771b5d50f9cae3717946c63af659510a1
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-03-14 23:50:45 +0530

    Add files via upload

---
+++ Case Study/01 Arithmetic Calculator/Helper/Calculator.c
@@ -0,0 +1,80 @@
+///Addition Function
+
+void Add()
+{
+    int N1 = 0, N2 = 0, Res = 0;
+    printf("\n Enter 1st Number = ");
+    scanf("%d",&N1);
+    printf("\n Enter 2nd Number = ");
+    scanf("%d",&N2);
+
+    Res = N1 + N2;
+
+    printf("\n Addition of %d + %d = %d",N1,N2,Res);
+    return;
+}
+
+ ///Substraction Function
+
+ void Sub()
+ {
+     int N1 = 0, N2 = 0, Res = 0;
+     printf("\n Enter 1st Number = ");
+     scanf("%d",&N1);
+     printf("\n Enter 2nd Number = ");
+     scanf("%d",&N2);
+
+     Res = N1 - N2;
+
+     printf("\n Substraction of %d - %d = %d",N1,N2,Res);
+     return;
+ }
+
+  ///Division Function
+
+  void Div()
+  {
+      int N1 = 0, N2 = 0, Res = 0;
+      printf("\n Enter 1st Number = ");
+      scanf("%d",&N1);
+      printf("\n Enter 2nd Number = ");
+      scanf("%d",&N2);
+
+      Res = N1 / N2;
+
+      printf("\n Division of %d / %d = %d",N1,N2,Res);
+      return;
+  }
+
+  ///Multiplication Function
+
+  void Mul()
+  {
+  int N1 = 0, N2 = 0, Res = 0;
+  printf("\n Enter 1st Number = ");
+  scanf("%d",&N1);
+  printf("\n Enter 2nd Number = ");
+  scanf("%d",&N2);
+
+  Res = N1 * N2;
+
+  printf("\n Multiplication of %d * %d = %d",N1,N2,Res);
+  return;
+  }
+
+  ///Modula Function
+
+  void Mod()
+  {
+      int N1 = 0, N2 = 0, Res = 0;
+      printf("\n Enter 1st Number = ");
+      scanf("%d",&N1);
+      printf("\n Enter 2nd Number = ");
+      scanf("%d",&N2);
+
+      Res = N1 % N2;
+
+      printf("\n Modula of %d + %d = %d",N1,N2,Res);
+      return;
+  }
+
