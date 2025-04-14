5676100b78794957a7666b24015615d386752296
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-04-14 23:46:23 +0530

    Add files via upload

---
+++ Control_Statement/01 Print Welcome 10 Times.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Cnt=0;
+    printf("\n ~~~~~Hello~~~~~~ \n");
+    for(Cnt=1; Cnt<=10; Cnt++)
+    {
+        printf("\n %3d WELCOME",Cnt);
+    }
+    printf("\n ===========THANKS==============");
+    getch();
+    return 0;
+}
