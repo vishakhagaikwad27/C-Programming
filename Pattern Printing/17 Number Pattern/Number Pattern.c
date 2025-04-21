e3619326be658ec9a7094492f92bafa564929277
vishakhagaikwad27 <vishakhagaikwad874@gmail.com>
2025-04-21 22:00:40 +0530

    Add files via upload

---
+++ Pattern Printing/17 Number Pattern/Number Pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i = 0, j = 0;
+
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 4; j >=1; j--)
+        {
+            printf(" %d ",j);
+        }
+        printf("\n");
+    }
+}
