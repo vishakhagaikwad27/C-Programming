5a203216a137b6dac6cb769000f2fd629879f8a8
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-05-08 23:42:34 +0530

    Add files via upload

---
+++ String/01 Basic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char City[20] = "Kolhapur City";
+
+    printf("\n String = %s.\n",City);
+
+    getch();
+    return 0;
+}
