a39f52ad36f3294fb356eb903de230461b64e3bf
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-12-30 21:15:46 +0530

    Print the letters in user specified range

---
+++ Control_Statement/33 [B] Write a program to print the letters in user specified range using a loop. (forward or reverse).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='\0', Spt='\0', Ept='\0';
+    printf("\n Enter a Starting Point = ");
+    scanf("%c",&Spt);
+
+    fflush(stdin);
+
+    printf("\n Enter a Ending Point = ");
+    scanf("%c",&Ept);
+
+    for(ch=Ept; ch>=Spt; ch--)
+    {
+        printf("\n %3c",ch);
+    }
+    printf("\n =========THANKS=========");
+    getch();
+    return 0;
+}
